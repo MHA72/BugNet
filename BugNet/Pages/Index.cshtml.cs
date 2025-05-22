b1e57e0609d73d3cab399c29a33ab57a951e81ad
Mohammad Hossein <hossinarab727@gmail.com>
2025-05-22 19:59:03 +0330

    Add Table

--- BugNet/Pages/Index.cshtml.cs
+++ BugNet/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using BugNet.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BugNet.Pages;
@@ -5,8 +6,16 @@ namespace BugNet.Pages;
 public class Index : PageModel
 {
     public string Title { get; set; } = "Main";
+    
+    public IList<Bug> Bugs { get; set; }
     public void OnGet()
     {
         ViewData["Title"] = Title;
+        Bugs = new List<Bug>
+        {
+            new() {Id = 1, Name = "Bug 1", Description = "Bug 1 description", IsDone = false},
+            new() {Id = 2, Name = "Bug 2", Description = "Bug 2 description", IsDone = false},
+            new() {Id = 3, Name = "Bug 3", Description = "Bug 3 description", IsDone = true},
+        };
     }
 }
