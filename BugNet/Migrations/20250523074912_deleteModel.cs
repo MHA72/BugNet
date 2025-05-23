6a66c9b546789e207be59ed0d580655a0bde88dc
Mohammad Hossein <hossinarab727@gmail.com>
2025-05-23 11:26:32 +0330

    Add Migrations

---
+++ BugNet/Migrations/20250523074912_deleteModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeleteTime",
+                table: "Bugs",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "Bugs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeleteTime",
+                table: "Bugs");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "Bugs");
+        }
+    }
+}
